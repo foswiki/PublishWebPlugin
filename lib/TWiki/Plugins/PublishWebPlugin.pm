6778e42dc56af752602e3e8b4bcc18484aa3f07d
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/PublishWebPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/PublishWebPlugin.pm
+++ lib/TWiki/Plugins/PublishWebPlugin.pm
@@ -10,7 +10,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -24,93 +24,92 @@ use strict;
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $pluginName $debug
-        $initialized $error $publishWeb $publishSkin $excludeTopic $homeLabel
-        $publishPath $attachPath $publishDir $attachDir
-    );
+  $web $topic $user $installWeb $VERSION $pluginName $debug
+  $initialized $error $publishWeb $publishSkin $excludeTopic $homeLabel
+  $publishPath $attachPath $publishDir $attachDir
+);
 
-$VERSION = '1.022';
-$pluginName = 'PublishWebPlugin';  # Name of this Plugin
+$VERSION     = '1.022';
+$pluginName  = 'PublishWebPlugin';    # Name of this Plugin
 $initialized = 0;
-$error = "";
+$error       = "";
 
 # Hardcoded settings (change if needed)
-$publishPath = "..";       # output dir; must be relative to twiki/pub
-$attachPath  = "_publish"; # attach dir; must be relative to $publishPath
+$publishPath = "..";          # output dir; must be relative to twiki/pub
+$attachPath  = "_publish";    # attach dir; must be relative to $publishPath
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.021 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1.021 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPluginPreferencesFlag( "DEBUG" );
-$debug=1;
+    $debug = TWiki::Func::getPluginPreferencesFlag("DEBUG");
+    $debug = 1;
 
-    writeDebug( "initPlugin( $web.$topic ) is OK" );
+    writeDebug("initPlugin( $web.$topic ) is OK");
     $initialized = 0;
-    $error = "";
+    $error       = "";
     return 1;
 }
 
-sub initialize
-{
-    return if( $initialized );
+sub initialize {
+    return if ($initialized);
 
     # Initialization
-    $publishDir = TWiki::Func::getPubDir( ) . '/' . $publishPath;
+    $publishDir = TWiki::Func::getPubDir() . '/' . $publishPath;
     $attachDir  = $publishDir . '/' . $attachPath;
 
     # Get plugin preferences
-    $publishWeb   = TWiki::Func::getPluginPreferencesValue( "PUBLISHWEBNAME" ) || "Publish";
-    $publishSkin  = TWiki::Func::getPluginPreferencesValue( "PUBLISHSKIN" ) || "print";
-    $excludeTopic = TWiki::Func::getPluginPreferencesValue( "EXCLUDETOPIC" ) || "";
+    $publishWeb = TWiki::Func::getPluginPreferencesValue("PUBLISHWEBNAME")
+      || "Publish";
+    $publishSkin = TWiki::Func::getPluginPreferencesValue("PUBLISHSKIN")
+      || "print";
+    $excludeTopic = TWiki::Func::getPluginPreferencesValue("EXCLUDETOPIC")
+      || "";
     $excludeTopic =~ s/,\s*/\|/go;
     $excludeTopic = '(' . $excludeTopic . ')';
-    $homeLabel    = TWiki::Func::getPluginPreferencesValue( "HOMELABEL" ) || "Home";
+    $homeLabel = TWiki::Func::getPluginPreferencesValue("HOMELABEL") || "Home";
 
     $initialized = 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    writeDebug( "commonTagsHandler( $_[2].$_[1] )" );
+    writeDebug("commonTagsHandler( $_[2].$_[1] )");
     $_[0] =~ s/%PUBLISHWEB{(.*?)}%/&handlePublish($1)/ge;
     $_[0] =~ s/%(START|STOP)PUBLISH%[\n\r]*//go;
 }
 
 # =========================
-sub afterSaveHandler
-{
+sub afterSaveHandler {
 ### my ( $text, $topic, $web, $error ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    writeDebug( "afterSaveHandler( $_[2].$_[1] )" );
+    writeDebug("afterSaveHandler( $_[2].$_[1] )");
 
-    # This handler is called by TWiki::Store::saveTopic just after the save action.
+ # This handler is called by TWiki::Store::saveTopic just after the save action.
     initialize();
-    return unless( $_[2] eq $publishWeb );
+    return unless ( $_[2] eq $publishWeb );
     publishTopic( $_[2], $_[1], $_[0] );
 }
 
 # =========================
-sub publishTopic
-{
-    my( $theWeb, $theTopic, $text ) = @_;
+sub publishTopic {
+    my ( $theWeb, $theTopic, $text ) = @_;
 
-    writeDebug( "publishTopic( $theWeb, $theTopic )" );
-    return unless( $theWeb eq $publishWeb );
-    return if( $theTopic =~ /$excludeTopic/ );
+    writeDebug("publishTopic( $theWeb, $theTopic )");
+    return unless ( $theWeb eq $publishWeb );
+    return if ( $theTopic =~ /$excludeTopic/ );
 
-    unless( $text ) {
+    unless ($text) {
         $text = TWiki::Func::readTopicText( $theWeb, $theTopic );
     }
     $text =~ s/%META:[A-Z0-9]+\{[^\n\r]+[\n\r]*//gs;
@@ -121,11 +120,14 @@ sub publishTopic
     $tmpl =~ s/%META\{.*?\}%[\n\r]*//gs;
     $tmpl =~ s/%TEXT%/$text/;
 
-    my $saveWeb   = $web;    $web   = $theWeb;
-    my $saveTopic = $topic;  $topic = $theTopic;
+    my $saveWeb = $web;
+    $web = $theWeb;
+    my $saveTopic = $topic;
+    $topic = $theTopic;
     $tmpl = TWiki::Func::expandCommonVariables( $tmpl, $topic, $web );
     ## FIXME my $wikiWordRegex = TWiki::Func::getRegularExpression( "wikiWordRegex" );
-    $tmpl =~ s/(^|[\(\s])([A-Z][A-Za-z0-9]*)\.([A-Z]+[a-z]+[A-Za-z0-9])/$1<nop>$3/go;
+    $tmpl =~
+      s/(^|[\(\s])([A-Z][A-Za-z0-9]*)\.([A-Z]+[a-z]+[A-Za-z0-9])/$1<nop>$3/go;
     $tmpl =~ s/\[\[(.*?)\]\[(.*?)\]\]/&handleLink($1,$2)/geo;
     $tmpl =~ s/\[\[(.*?)\]\]/&handleLink($1,$1)/geo;
     $tmpl = TWiki::Func::renderText( $tmpl, $web );
@@ -133,11 +135,12 @@ sub publishTopic
     # fix links to attachments
     my $pubDir = TWiki::Func::getPubDir();
     my $pubUrl = TWiki::Func::getPubUrlPath();
-    $tmpl =~ s/($pubUrl)\/([^'" ]+)/&fixAndCopyAttachments($1, $2, $pubDir )/geo;
+    $tmpl =~
+      s/($pubUrl)\/([^'" ]+)/&fixAndCopyAttachments($1, $2, $pubDir )/geo;
     $tmpl =~ s/<\/?(nop|noautolink)\/?>\n?//gois;
 
     my $name = buildName( $topic, 'file' );
-    writeDebug( "publishTopic, saving file $name using $publishSkin skin" );
+    writeDebug("publishTopic, saving file $name using $publishSkin skin");
     TWiki::Func::saveFile( $name, $tmpl );
 
     $web   = $saveWeb;
@@ -146,104 +149,118 @@ sub publishTopic
 }
 
 # =========================
-sub fixAndCopyAttachments
-{
+sub fixAndCopyAttachments {
     my ( $pubUrl, $path, $pubDir ) = @_;
     my $link = "/$attachPath/$path";
     my $file = $path;
     $file =~ s/.*\///;
     my $from = "$pubDir/$path";
     my $to   = "$attachDir/$file";
+
 #    writeDebug( "fixAndCopyAttachments, copying attachment from $from to $to" );
     use File::Copy;
-    unless( copy( $from, $to ) ) {
+    unless ( copy( $from, $to ) ) {
         $error = "Error: Can't copy $from $to ($!)";
-        TWiki::Func::writeWarning( "- ${pluginName}: $error\n" );
+        TWiki::Func::writeWarning("- ${pluginName}: $error\n");
     }
     return "/$attachPath/$file";
 }
 
 # =========================
-sub handleLink
-{
+sub handleLink {
     my ( $link, $label ) = @_;
-    if( $link =~ /^(http|ftp)\:/ ) {
+    if ( $link =~ /^(http|ftp)\:/ ) {
         return "<a href=\"$link\">$label</a>";
-    } elsif( $link eq $label ) {
-        return '<a href="'
-               . buildName( $link, 'url' ) . '">'
-               . buildName( $link, 'label' ) . '</a>';
-    } else {
+    }
+    elsif ( $link eq $label ) {
+        return
+            '<a href="'
+          . buildName( $link, 'url' ) . '">'
+          . buildName( $link, 'label' ) . '</a>';
+    }
+    else {
         return '<a href="' . buildName( $link, 'url' ) . "\">$label</a>";
     }
 }
 
 # =========================
-sub handlePublish
-{
-    my ( $attr ) = @_;
-    my $action = TWiki::Func::extractNameValuePair( $attr );
-    my $text = '';
+sub handlePublish {
+    my ($attr) = @_;
+    my $action = TWiki::Func::extractNameValuePair($attr);
+    my $text   = '';
     initialize();
-    if( $action eq "breadcrumb" ) {
+    if ( $action eq "breadcrumb" ) {
         $text = '';
-        if( $topic ne "Index" ) {
+        if ( $topic ne "Index" ) {
             $text .= "[[Index][$homeLabel]]";
-            foreach( getParents( $web, $topic ) ) {
-                $text .= " &gt; [[$_]["
-                      . buildName( $_, 'label' ) . ']]';
+            foreach ( getParents( $web, $topic ) ) {
+                $text .= " &gt; [[$_][" . buildName( $_, 'label' ) . ']]';
             }
             $text .= ' &gt; ';
         }
-    } elsif( $action eq "nicetopic" ) {
-        $text =  buildName( $topic, 'label' );
-    } elsif( $action eq "topicname" ) {
-        $text =  buildName( $topic, 'link' );
-    } elsif( $action eq "topicurl" ) {
-        $text =  buildName( $topic, 'url' );
-    } elsif( $action eq "publish" ) {
+    }
+    elsif ( $action eq "nicetopic" ) {
+        $text = buildName( $topic, 'label' );
+    }
+    elsif ( $action eq "topicname" ) {
+        $text = buildName( $topic, 'link' );
+    }
+    elsif ( $action eq "topicurl" ) {
+        $text = buildName( $topic, 'url' );
+    }
+    elsif ( $action eq "publish" ) {
         my $topicName = TWiki::Func::extractNameValuePair( $attr, "topic" );
-        if( $topicName eq "all" ) {
+        if ( $topicName eq "all" ) {
             my @topics = ();
-            foreach( TWiki::Func::getTopicList( $publishWeb ) ) {
+            foreach ( TWiki::Func::getTopicList($publishWeb) ) {
                 $topicName = $_;
-                if( publishTopic( $publishWeb, $topicName ) ) {
+                if ( publishTopic( $publishWeb, $topicName ) ) {
                     push( @topics, "[[$publishWeb.$topicName]]" );
                 }
             }
             my $done = join( ', ', @topics );
             $text = "PUBLISHWEB: Published topics $done";
-        } elsif( $topicName ) {
-            if( TWiki::Func::topicExists( $publishWeb, $topicName ) ) {
-                if( publishTopic( $publishWeb, $topicName ) ) {
-                    $text = "PUBLISHWEB: Published topic [[$publishWeb.$topicName]]";
-                } else {
-                    $text = "PUBLISHWEB error: Topic [[$publishWeb.$topicName]] not published";
-                }
-            } else {
-                $text = "PUBLISHWEB error: Topic <nop>$publishWeb.$topicName does not exist";
-            }
-        } else {
-            $text = 'PUBLISHWEB error: Missing topic="" parameter for "publish" action';
         }
-    } elsif( $action ) {
+        elsif ($topicName) {
+            if ( TWiki::Func::topicExists( $publishWeb, $topicName ) ) {
+                if ( publishTopic( $publishWeb, $topicName ) ) {
+                    $text =
+                      "PUBLISHWEB: Published topic [[$publishWeb.$topicName]]";
+                }
+                else {
+                    $text =
+"PUBLISHWEB error: Topic [[$publishWeb.$topicName]] not published";
+                }
+            }
+            else {
+                $text =
+"PUBLISHWEB error: Topic <nop>$publishWeb.$topicName does not exist";
+            }
+        }
+        else {
+            $text =
+'PUBLISHWEB error: Missing topic="" parameter for "publish" action';
+        }
+    }
+    elsif ($action) {
         $text = 'PUBLISHWEB error: Unrecognized action';
-    } else {
+    }
+    else {
         $text = '';
     }
     return $text;
 }
 
 # =========================
-sub getParents
-{
+sub getParents {
     my ( $web, $topic ) = @_;
-    my @arr = ( );
-    for(;;) {
+    my @arr = ();
+    for ( ; ; ) {
         my $text = TWiki::Func::readTopicText( $web, $topic, '', 1 );
-        last unless( $text =~ s/.*?\%META:TOPICPARENT\{name\=\"([^\"]+).*/$1/s );
-        last if( $text =~ /^(Index|WebHome)$/ ); # stop at home topic
-        last if( grep { /^$text$/ } @arr );      # prevent recursion
+        last
+          unless ( $text =~ s/.*?\%META:TOPICPARENT\{name\=\"([^\"]+).*/$1/s );
+        last if ( $text =~ /^(Index|WebHome)$/ );    # stop at home topic
+        last if ( grep { /^$text$/ } @arr );         # prevent recursion
         push( @arr, $text );
         $topic = $text;
     }
@@ -251,23 +268,25 @@ sub getParents
 }
 
 # =========================
-sub buildName
-{
+sub buildName {
     my ( $topic, $type ) = @_;
+
     # $type for 'Topic_Name':
     # 'name':   'topic_name.html'
     # 'url':    '/topic_name.html'
     # 'file':   '/file/path/to/topic_name.html'
     # 'label':  'Topic Name'
-    my $text = lc( $topic ) . '.html';
+    my $text = lc($topic) . '.html';
     $text =~ s/[^a-z_\.]+//go;
     $text =~ /(.*)/;
-    $text = $1; # untaint
-    if( $type eq 'url' ) {
+    $text = $1;    # untaint
+    if ( $type eq 'url' ) {
         $text = '/' . $text;
-    } elsif( $type eq 'file' ) {
+    }
+    elsif ( $type eq 'file' ) {
         $text = $publishDir . '/' . $text;
-    } elsif( $type eq 'label' ) {
+    }
+    elsif ( $type eq 'label' ) {
         $text = $topic;
         $text =~ s/_/ /go;
         $text =~ s/^Index$/$homeLabel/o;
@@ -276,10 +295,9 @@ sub buildName
 }
 
 # =========================
-sub writeDebug
-{
-    my( $text ) = @_;
-    TWiki::Func::writeDebug( "- ${pluginName}: $text" ) if $debug;
+sub writeDebug {
+    my ($text) = @_;
+    TWiki::Func::writeDebug("- ${pluginName}: $text") if $debug;
 }
 
 # =========================
